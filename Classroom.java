b18eb324ad57f745cc2510050d5b874c47223cc2
강성민 <sungmin@manseo.localdomain>
2024-06-13 23:27:00 +0900

    Final Version of SeatPlan

--- Classroom.java
+++ Classroom.java
@@ -1,23 +1,71 @@
 package JavaProject;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class Classroom {
-    //분단 배열(Division이 들어갈 배열)
-    private ArrayList<Division> divisionArrayList;
+public class Classroom{
+    private ArrayList<Division> divisions;
+    private int rowNum, colNum;
 
-    // 전체 학생 수, 분단의 개수, 분단 가로개수, 분단 세로개수
-    public Classroom(int numOfStudent, int numOfDivision, int numOfRows, int numOfColumns) {
-        this.divisionArrayList = new ArrayList<>();
-        
-        for(int i=0; i < numOfDivision; i++) {
-        	Division division = new Division(numOfRows, numOfColumns);
-        	this.divisionArrayList.add(division);
+    public ArrayList<Division> getDivisions() {return divisions;}
+
+    public Classroom(int divisionNum, int rowNum, int colNum){
+        //make divisions
+        divisions = new ArrayList<>();
+        this.rowNum = rowNum; this.colNum = colNum;
+        for(int i = 0; i < divisionNum; i++){divisions.add(new Division(rowNum, colNum));}
+    }
+
+    void seatRandomly(ArrayList<Student> students){
+        Collections.shuffle(students);
+        int seatedStudents = 0;
+        for(int i = 0; i < rowNum; i++){
+            for(int j = 0; j < divisions.size(); j++){
+                for(int k = 0; k < colNum; k++){
+                    if(seatedStudents < students.size()){
+                        Student myStudent = students.get(seatedStudents);
+                        divisions.get(j).getSeats()[i][k].setMyStudent(myStudent); seatedStudents++; divisions.get(j).getSeats()[i][k].setOccupied(true);
+                        System.out.println(myStudent.getMyName() + ", 남아있는학생수: " + (students.size() - seatedStudents));
+                    }
+                }
+            }
         }
     }
-    
-    // 반환
-    public ArrayList<Division> getDivisionArrayList(){
-    	return this.divisionArrayList;
+
+    void seatRandomlyMixedVer(ArrayList<Student> students){
+        Collections.shuffle(students);
+        int seatedStudents = 0;
+        int seatedBoys = 0;
+        int seatedGirls = 0;
+        ArrayList<Student> boys = new ArrayList<>();
+        ArrayList<Student> girls = new ArrayList<>();
+
+        //init boys and girls info
+        for(Student myStudent : students){
+            if(myStudent.getMygender().equals(Main.gender.MALE)){boys.add(myStudent);}
+            else if (myStudent.getMygender().equals(Main.gender.FEMALE)){girls.add(myStudent);}
+        }
+        //seat randomly mixed ver
+        for(int i = 0; i < rowNum; i++){
+            for(int j = 0; j < divisions.size(); j++){
+                for(int k = 0; k < colNum; k++){
+                    if(seatedStudents < students.size()){
+                        Student myStudent;
+                        if(seatedStudents % 2 == 0){
+                            if(seatedBoys < boys.size()){myStudent = boys.get(seatedBoys); seatedBoys++;}
+                            else {myStudent = girls.get(seatedGirls); seatedGirls++;}
+                        }
+                        else{
+                            if(seatedGirls < girls.size()){myStudent = girls.get(seatedGirls); seatedGirls++;}
+                            else{myStudent = boys.get(seatedBoys); seatedBoys++;}
+                        }
+                        divisions.get(j).getSeats()[i][k].setMyStudent(myStudent); seatedStudents++; divisions.get(j).getSeats()[i][k].setOccupied(true);
+                        System.out.println(myStudent.getMyName() + ", 남아있는학생수: " + (students.size() - seatedStudents));
+                    }
+                }
+            }
+        }
+
     }
+
 }
--- Division.java
+++ Division.java
@@ -1,23 +1,22 @@
 package JavaProject;
 
-public class Division {
-    private int numOfRows; // 2
-    private int numOfColumns; // 4
-    private Seat[][] detailOfDivision; // 2차원 배열
+public class Division{
+    private Seat[][] seats;
 
-    public Division(int numOfRows, int numOfColumns) {
-        this.numOfRows = numOfRows;
-        this.numOfColumns = numOfColumns;
-        this.detailOfDivision = new Seat[numOfRows][numOfColumns]; // 분단을 2차원 배열로 생성
-        
-        for(int i=0; i<numOfRows; i++) {
-        	for(int j=0; j<numOfColumns; j++) {
-        		this.detailOfDivision[i][j] = new Seat();
-        	}
+    public Division(int rowNum, int colNum){
+        this.seats = new Seat[rowNum][colNum];
+        for (int i = 0; i < rowNum; i++) {
+            for (int j = 0; j < colNum; j++) {
+                seats[i][j] = new Seat();
+            }
         }
     }
-    
-    public Seat[][] getDetailOfDivision() {
-        return this.detailOfDivision;
+
+    public Seat[][] getSeats() {
+        return seats;
+    }
+
+    public void setSeats(Seat[][] seats) {
+        this.seats = seats;
     }
 }
